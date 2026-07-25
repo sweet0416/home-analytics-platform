8f031fec51970a2183b40700d986565f8507097e
Edison <edison_fm@outlook.com>
2026-07-25 23:57:27 +0800

    fix: update lottery radio button values

--- frontend/src/plugins/lottery/views/HeatmapView.vue
+++ frontend/src/plugins/lottery/views/HeatmapView.vue
@@ -7,8 +7,8 @@
       </div>
       <div class="heatmap-actions">
         <el-radio-group v-model="selectedArea" size="small" @change="handleAreaChange">
-          <el-radio-button label="front">前区</el-radio-button>
-          <el-radio-button label="back">后区</el-radio-button>
+          <el-radio-button value="front">前区</el-radio-button>
+          <el-radio-button value="back">后区</el-radio-button>
         </el-radio-group>
         <el-segmented v-model="selectedMetric" :options="metricOptions" size="small" />
         <el-button :icon="Refresh" :loading="heatmapLoading" @click="reloadHeatmap">
--- frontend/src/plugins/lottery/views/OmissionView.vue
+++ frontend/src/plugins/lottery/views/OmissionView.vue
@@ -7,8 +7,8 @@
       </div>
       <div class="omission-actions">
         <el-radio-group v-model="selectedArea" size="small" @change="handleAreaChange">
-          <el-radio-button label="front">前区</el-radio-button>
-          <el-radio-button label="back">后区</el-radio-button>
+          <el-radio-button value="front">前区</el-radio-button>
+          <el-radio-button value="back">后区</el-radio-button>
         </el-radio-group>
         <el-button :icon="Refresh" :loading="omissionLoading" @click="reloadOmissions">
           刷新
